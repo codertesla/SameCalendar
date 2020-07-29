27e0f261213a6fa57b465462d22d3defd35ab39e
codertesla <hat586@gmail.com>
2020-07-29 17:41:13 +0800

    print pretty message for lucky years.

--- SameCanlendar.py
+++ SameCanlendar.py
@@ -12,7 +12,7 @@ def get_lucky_years(this_year):
         lucky_year = this_year + i
         lucky_year_date = [calendar.monthrange(lucky_year, m) for m in range(1,13)] # date of the lucky year
         if lucky_year_date == this_year_date:
-            print(f'AHA! Found a lucky year to reuse your calendar: {lucky_year}')
+            # print(f'AHA! Found a lucky year to reuse your {this_year} calendar: {lucky_year}')
             lucky_years.append(lucky_year)
         i += 1
         continue
@@ -22,11 +22,15 @@ def get_lucky_years(this_year):
 
 def main():
     this_year = datetime.datetime.now().year # this year
-    lucky_years = get_lucky_years(this_year)
-    print(lucky_years)
+    lucky_years = get_lucky_years(this_year) # lucky years with the same calendar
+    
+    years_before = ', '.join([str(year) for year in lucky_years if year < this_year])
+    years_after = ', '.join([str(year) for year in lucky_years if year > this_year])
+
+    print(f'\nCongrats! You can reuse your {this_year} calendar in {years_after}.')
+    print(f'\nWait... BTW, You can reuse your {years_before} calendars this year.')
+
     return
 
 if __name__ == "__main__":
     main()
-
-
